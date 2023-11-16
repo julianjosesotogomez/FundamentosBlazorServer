92d92701debdf65c3246f75801eaf4c8c2ce9362
Julian José Soto Gómez <76453216+julianjosesotogomez@users.noreply.github.com>
2023-11-16 11:30:00 -0500

    Implementación de Code Behind - Codigo de segundo plano

---
+++ Pages/CodeBehind.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace FundamentosBlazorServer.Pages
+{
+    public partial class CodeBehind
+    {
+        private bool viewMessaje = false;
+
+        private void ViewMessage()
+        {
+            viewMessaje = true;
+        }
+    }
+}
